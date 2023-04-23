2cd4f2bc6d3db1cf1c5dc820b403eaca3b69d87d
retronouns <3486507+retronouns@users.noreply.github.com>
2023-04-23 01:20:42 -0600

    test for numbers and extra whitespace in outer tokenizer

--- src/token/token.test.ts
+++ src/token/token.test.ts
@@ -3,7 +3,7 @@ import { ParseString, ParseWord, ParseToken, ParseTokens } from './token'
 
 const VERY_LONG_STRING = `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'`
 const VERY_LONG_WORDS = `Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book It has survived not only five centuries but also the leap into electronic typesetting remaining essentially unchanged It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`
-const VERY_LONG_TOKENS = `Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown 'i\\'m a string' took a galley of type and scrambled it to make a type specimen book It has survived not only five centuries but also the leap into electronic typesetting remaining essentially unchanged It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 'i\\'m a string'`
+const VERY_LONG_TOKENS = `Lorem  Ipsum   1 is 12 simply 123 dummy \n text\n\n\n of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown 'i\\'m a string' took a galley of type and scrambled it to make a type specimen book It has survived not only five centuries but also the leap into electronic typesetting remaining essentially unchanged It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 'i\\'m a string'`
 
 /**
  * ParseString Tests
@@ -215,8 +215,11 @@ expectToBe<
 >([
     `Lorem`,
     `Ipsum`,
+    `1`,
     `is`,
+    `12`,
     `simply`,
+    `123`,
     `dummy`,
     `text`,
     `of`,
@@ -308,8 +311,11 @@ expectToBe<
     `,`,
     `Lorem`,
     `Ipsum`,
+    `1`,
     `is`,
+    `12`,
     `simply`,
+    `123`,
     `dummy`,
     `text`,
     `of`,
